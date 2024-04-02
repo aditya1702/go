9b3deb9f28ddaca77541fbb0f316d46d4aaa8e31
Aditya Vyas <aditya.vyas@stellar.org>
2024-04-02 14:28:28 -0400

    Group metrics into submission duration

--- clients/stellarcore/metrics_client.go
+++ clients/stellarcore/metrics_client.go
@@ -20,10 +20,6 @@ type clientWithMetrics struct {
 		// submissions to stellar-core
 		SubmissionDuration *prometheus.SummaryVec
 
-		// SubmissionsCounter tracks the rate of transactions that have
-		// been submitted to this process
-		SubmissionsCounter *prometheus.CounterVec
-
 		// V0TransactionsCounter tracks the rate of v0 transaction envelopes that
 		// have been submitted to this process
 		V0TransactionsCounter *prometheus.CounterVec
@@ -49,24 +45,23 @@ func (c *clientWithMetrics) SubmitTransaction(ctx context.Context, rawTx string,
 func (c *clientWithMetrics) updateTxSubMetrics(duration float64, envelope xdr.TransactionEnvelope, response *proto.TXResponse, err error) {
 	var label prometheus.Labels
 	if err != nil {
-		label = prometheus.Labels{"status": "request_error"}
+		label["status"] = "request_error"
 	} else if response.IsException() {
-		label = prometheus.Labels{"status": "exception"}
+		label["status"] = "exception"
 	} else {
-		label = prometheus.Labels{"status": response.Status}
+		label["status"] = response.Status
 	}
-
-	c.TxSubMetrics.SubmissionDuration.With(label).Observe(duration)
-	c.TxSubMetrics.SubmissionsCounter.With(label).Inc()
 
 	switch envelope.Type {
 	case xdr.EnvelopeTypeEnvelopeTypeTxV0:
-		c.TxSubMetrics.V0TransactionsCounter.With(label).Inc()
+		label["envelope_type"] = "v0"
 	case xdr.EnvelopeTypeEnvelopeTypeTx:
-		c.TxSubMetrics.V1TransactionsCounter.With(label).Inc()
+		label["envelope_type"] = "v1"
 	case xdr.EnvelopeTypeEnvelopeTypeTxFeeBump:
-		c.TxSubMetrics.FeeBumpTransactionsCounter.With(label).Inc()
+		label["envelope_type"] = "fee-bump"
 	}
+
+	c.TxSubMetrics.SubmissionDuration.With(label).Observe(duration)
 }
 
 func NewClientWithMetrics(client Client, registry *prometheus.Registry, prometheusSubsystem string) ClientWithMetrics {
@@ -76,54 +71,21 @@ func NewClientWithMetrics(client Client, registry *prometheus.Registry, promethe
 		Name:       "submission_duration_seconds",
 		Help:       "submission durations to Stellar-Core, sliding window = 10m",
 		Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
-	}, []string{"status"})
-	submissionsCounter := prometheus.NewCounterVec(prometheus.CounterOpts{
-		Namespace: "horizon",
-		Subsystem: prometheusSubsystem,
-		Name:      "submissions_count",
-		Help:      "number of submissions, sliding window = 10m",
-	}, []string{"status"})
-	v0TransactionsCounter := prometheus.NewCounterVec(prometheus.CounterOpts{
-		Namespace: "horizon",
-		Subsystem: prometheusSubsystem,
-		Name:      "v0_count",
-		Help:      "number of v0 transaction envelopes submitted, sliding window = 10m",
-	}, []string{"status"})
-	v1TransactionsCounter := prometheus.NewCounterVec(prometheus.CounterOpts{
-		Namespace: "horizon",
-		Subsystem: prometheusSubsystem,
-		Name:      "v1_count",
-		Help:      "number of v1 transaction envelopes submitted, sliding window = 10m",
-	}, []string{"status"})
-	feeBumpTransactionsCounter := prometheus.NewCounterVec(prometheus.CounterOpts{
-		Namespace: "horizon",
-		Subsystem: prometheusSubsystem,
-		Name:      "feebump_count",
-		Help:      "number of fee bump transaction envelopes submitted, sliding window = 10m",
-	}, []string{"status"})
+	}, []string{"status", "envelope_type"})
 
 	registry.MustRegister(
 		submissionDuration,
-		submissionsCounter,
-		v0TransactionsCounter,
-		v1TransactionsCounter,
-		feeBumpTransactionsCounter,
 	)
 
 	return &clientWithMetrics{
 		CoreClient: client,
 		TxSubMetrics: struct {
 			SubmissionDuration         *prometheus.SummaryVec
-			SubmissionsCounter         *prometheus.CounterVec
 			V0TransactionsCounter      *prometheus.CounterVec
 			V1TransactionsCounter      *prometheus.CounterVec
 			FeeBumpTransactionsCounter *prometheus.CounterVec
 		}{
-			SubmissionDuration:         submissionDuration,
-			SubmissionsCounter:         submissionsCounter,
-			V0TransactionsCounter:      v0TransactionsCounter,
-			V1TransactionsCounter:      v1TransactionsCounter,
-			FeeBumpTransactionsCounter: feeBumpTransactionsCounter,
+			SubmissionDuration: submissionDuration,
 		},
 	}
 }

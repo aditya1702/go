28e392ab508c4254d41a8d4e664b66a3913ed615
Aditya Vyas <aditya.vyas@stellar.org>
2024-04-25 14:17:22 -0400

    Fix failing submitter_test

--- services/horizon/internal/txsub/submitter_test.go
+++ services/horizon/internal/txsub/submitter_test.go
@@ -10,6 +10,10 @@ import (
 	"github.com/stellar/go/services/horizon/internal/test"
 )
 
+const (
+	TxXDR = "AAAAAAGUcmKO5465JxTSLQOQljwk2SfqAJmZSG6JH6wtqpwhAAABLAAAAAAAAAABAAAAAAAAAAEAAAALaGVsbG8gd29ybGQAAAAAAwAAAAAAAAAAAAAAABbxCy3mLg3hiTqX4VUEEp60pFOrJNxYM1JtxXTwXhY2AAAAAAvrwgAAAAAAAAAAAQAAAAAW8Qst5i4N4Yk6l+FVBBKetKRTqyTcWDNSbcV08F4WNgAAAAAN4Lazj4x61AAAAAAAAAAFAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABLaqcIQAAAEBKwqWy3TaOxoGnfm9eUjfTRBvPf34dvDA0Nf+B8z4zBob90UXtuCqmQqwMCyH+okOI3c05br3khkH0yP4kCwcE"
+)
+
 func TestDefaultSubmitter(t *testing.T) {
 	ctx := test.Context()
 	// submits to the configured stellar-core instance correctly
@@ -20,10 +24,10 @@ func TestDefaultSubmitter(t *testing.T) {
 	defer server.Close()
 
 	s := NewDefaultSubmitter(http.DefaultClient, server.URL, prometheus.NewRegistry())
-	sr := s.Submit(ctx, "hello")
+	sr := s.Submit(ctx, TxXDR)
 	assert.Nil(t, sr.Err)
 	assert.True(t, sr.Duration > 0)
-	assert.Equal(t, "hello", server.LastRequest.URL.Query().Get("blob"))
+	assert.Equal(t, TxXDR, server.LastRequest.URL.Query().Get("blob"))
 
 	// Succeeds when stellar-core gives the DUPLICATE response.
 	server = test.NewStaticMockServer(`{
@@ -33,24 +37,24 @@ func TestDefaultSubmitter(t *testing.T) {
 	defer server.Close()
 
 	s = NewDefaultSubmitter(http.DefaultClient, server.URL, prometheus.NewRegistry())
-	sr = s.Submit(ctx, "hello")
+	sr = s.Submit(ctx, TxXDR)
 	assert.Nil(t, sr.Err)
 
 	// Errors when the stellar-core url is empty
 
 	s = NewDefaultSubmitter(http.DefaultClient, "", prometheus.NewRegistry())
-	sr = s.Submit(ctx, "hello")
+	sr = s.Submit(ctx, TxXDR)
 	assert.NotNil(t, sr.Err)
 
 	//errors when the stellar-core url is not parseable
 
 	s = NewDefaultSubmitter(http.DefaultClient, "http://Not a url", prometheus.NewRegistry())
-	sr = s.Submit(ctx, "hello")
+	sr = s.Submit(ctx, TxXDR)
 	assert.NotNil(t, sr.Err)
 
 	// errors when the stellar-core url is not reachable
 	s = NewDefaultSubmitter(http.DefaultClient, "http://127.0.0.1:65535", prometheus.NewRegistry())
-	sr = s.Submit(ctx, "hello")
+	sr = s.Submit(ctx, TxXDR)
 	assert.NotNil(t, sr.Err)
 
 	// errors when the stellar-core returns an unparseable response
@@ -58,7 +62,7 @@ func TestDefaultSubmitter(t *testing.T) {
 	defer server.Close()
 
 	s = NewDefaultSubmitter(http.DefaultClient, server.URL, prometheus.NewRegistry())
-	sr = s.Submit(ctx, "hello")
+	sr = s.Submit(ctx, TxXDR)
 	assert.NotNil(t, sr.Err)
 
 	// errors when the stellar-core returns an exception response
@@ -66,7 +70,7 @@ func TestDefaultSubmitter(t *testing.T) {
 	defer server.Close()
 
 	s = NewDefaultSubmitter(http.DefaultClient, server.URL, prometheus.NewRegistry())
-	sr = s.Submit(ctx, "hello")
+	sr = s.Submit(ctx, TxXDR)
 	assert.NotNil(t, sr.Err)
 	assert.Contains(t, sr.Err.Error(), "Invalid XDR")
 
@@ -75,7 +79,7 @@ func TestDefaultSubmitter(t *testing.T) {
 	defer server.Close()
 
 	s = NewDefaultSubmitter(http.DefaultClient, server.URL, prometheus.NewRegistry())
-	sr = s.Submit(ctx, "hello")
+	sr = s.Submit(ctx, TxXDR)
 	assert.NotNil(t, sr.Err)
 	assert.Contains(t, sr.Err.Error(), "NOTREAL")
 
@@ -84,7 +88,7 @@ func TestDefaultSubmitter(t *testing.T) {
 	defer server.Close()
 
 	s = NewDefaultSubmitter(http.DefaultClient, server.URL, prometheus.NewRegistry())
-	sr = s.Submit(ctx, "hello")
+	sr = s.Submit(ctx, TxXDR)
 	assert.IsType(t, &FailedTransactionError{}, sr.Err)
 	ferr := sr.Err.(*FailedTransactionError)
 	assert.Equal(t, "1234", ferr.ResultXDR)
